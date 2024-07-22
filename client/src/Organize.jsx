367eb1f52ce8a4322f85785b4f12683167918817
temp <maheshvengala4321@gmail.com>
2024-07-22 20:34:14 +0530

    addpost added

--- client/src/Organize.jsx
+++ client/src/Organize.jsx
@@ -1,25 +1,24 @@
 import React, { useState, useEffect } from "react";
 import org from "./Organize.module.css";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import "./Temp.css";
 import { API_URL } from "./data/apipath";
-import Model from 'react-modal'
+import Model from "react-modal";
+import { useNavigate } from "react-router-dom";
 
 const Organize = () => {
   const [isSignIn, setIsSignIn] = useState(true);
-  const [Organization,setOrganization] = useState('');
-  const [email,setemail] =useState('');
-  const [password,setpassword] = useState('');
-  const [Email,setEmail] = useState('');
-  const [Password,setPassword] = useState('');
-  const [visible,setvisible] = useState(false);
-  const [Emails,setEmails] =useState('');
-  const [OTP,setOTP] = useState('');
-  const [newPassword,setnewPassword] =useState('')
-  Model.setAppElement('#root')
-
-
-
+  const [Organization, setOrganization] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [visible, setvisible] = useState(false);
+  const [Emails, setEmails] = useState("");
+  const [OTP, setOTP] = useState("");
+  const [newPassword, setnewPassword] = useState("");
+  const navigate = useNavigate();
+  Model.setAppElement("#root");
 
   useEffect(() => {
     setTimeout(() => {
@@ -31,124 +30,115 @@ const Organize = () => {
     setIsSignIn(!isSignIn);
   };
 
-  const mess = document.getElementById('error');
-  const logmess = document.getElementById('logmess');
-  const otp =document.getElementById('otp');
-  const emailcheck = document.getElementById('emailcheck');
+  const mess = document.getElementById("error");
+  const logmess = document.getElementById("logmess");
+  const otp = document.getElementById("otp");
+  const emailcheck = document.getElementById("emailcheck");
 
-  const handleSubmit = async (e)=> {
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
     try {
-      const response = await fetch(`${API_URL}/college/collegeSignup`,{
-        method:'POST',
-        headers:{
-          'content-type':'application/json'
+      const response = await fetch(`${API_URL}/college/collegeSignup`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
         },
-        body:JSON.stringify({Organization,email,password})
-      })
+        body: JSON.stringify({ Organization, email, password }),
+      });
 
       const data = await response.json();
 
-      if(response.ok){
+      if (response.ok) {
         console.log(data);
-        alert('Registration Successful');
+        alert("Registration Successful");
         setIsSignIn(true);
-      }
-      else{
-        mess.textContent= "Email already Exists"
+      } else {
+        mess.textContent = "Email already Exists";
       }
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
-  const handleLogin = async (e)=> {
-    e.preventDefault()
-
-    try {
-      const response = await fetch(`${API_URL}/college/collegeLogin`,{
-        method:'POST',
-        headers:{
-          'content-type':'application/json'
-        },
-        body:JSON.stringify({Email,Password})
-      })
-
-      const data = await response.json()
-
-
-      if(response.ok){
-        console.log(data)
-        alert('Login successful')
-        localStorage.setItem('collegeToken',data.token)
-      }
-      logmess.textContent="Invalid Email or password"
-
-
-    } catch (error) {
-      
-    }
-  }
-  const handleOtp =async (e)=> {
-    e.preventDefault();
-      try {
-        const response = await fetch(`${API_URL}/college/collegeotp`,{
-          method:'POST',
-          headers:{
-            'content-type':'application/json'
-          },
-          body:JSON.stringify({Emails})
-        })
-
-        const data = await response.json();
-
-        if(response.ok){
-          console.log(data);
-          otp.textContent="* OTP send successfully"
-
-        }
-        else{
-          emailcheck.textContent="Invalid Email id"
-        }
-      } catch (error) {
-        console.error(error);
-      }
-
-  }  
-
-
-  const handleResetPassword = async (e)=> {
+  const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await fetch(`${API_URL}/college/collegepassreset`,{
-        method:'POST',
-        headers:{
-          'content-type':'application/json'
+      const response = await fetch(`${API_URL}/college/collegeLogin`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
         },
-        body:JSON.stringify({Emails,OTP,newPassword})
-      })
+        body: JSON.stringify({ Email, Password }),
+      });
 
       const data = await response.json();
 
-      if(response.ok){
+      if (response.ok) {
         console.log(data);
-        alert('Your password reset successfully');
+        alert("Login successful");
+        localStorage.setItem("collegeToken", data.token);
+        navigate("/myposts");
+      }
+      logmess.textContent = "Invalid Email or password";
+    } catch (error) {}
+  };
+  const handleOtp = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${API_URL}/college/collegeotp`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Emails }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log(data);
+        otp.textContent = "* OTP send successfully";
+      } else {
+        emailcheck.textContent = "Invalid Email id";
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`${API_URL}/college/collegepassreset`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ Emails, OTP, newPassword }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log(data);
+        alert("Your password reset successfully");
         setvisible(false);
       }
 
-      otp.textContent="An error occured please try again"
+      otp.textContent = "An error occured please try again";
     } catch (error) {
       console.error(error);
     }
-  }
-
+  };
 
   return (
     <div
       id="container"
-      className={`${org.container} ${isSignIn ? org.signin : org.signup}`} style={{ zIndex: visible ? -1 : '6' }}
+      className={`${org.container} ${isSignIn ? org.signin : org.signup}`}
+      style={{ zIndex: visible ? -1 : "6" }}
     >
       <br />
       <div className={org.navbar}>
@@ -166,32 +156,50 @@ const Organize = () => {
         >
           <div className={`${org.formwrapper} ${org.alignitemscenter}`}>
             <div className={`${org.form} ${org.signup}`}>
-            <form action="" onSubmit={handleSubmit}>  
-              <div className={org.inputgroup}>
-                <i className={`${org.bx} ${org.bxsuser}`}></i>
-                <p id="error"></p>
-                <input type="text" placeholder="Organization Name"  value={Organization} onChange={(e)=> setOrganization(e.target.value)} required/>
-              </div>
-              <div className={org.inputgroup}>
-                <i className={`${org.bx} ${org.bxmailsend}`}></i>
-                <input type="email" placeholder="Email" value={email} onChange={(e) => setemail(e.target.value)} required/>
-              </div>
-              <div className={org.inputgroup}>
-                <i className={`${org.bx} ${org.bxslockalt}`}></i>
-                <input type="password" placeholder="Password" value={password} onChange={(e)=> setpassword(e.target.value)} required/>
-              </div>
-              {/* <div className={org.inputgroup}>
+              <form action="" onSubmit={handleSubmit}>
+                <div className={org.inputgroup}>
+                  <i className={`${org.bx} ${org.bxsuser}`}></i>
+                  <p id="error"></p>
+                  <input
+                    type="text"
+                    placeholder="Organization Name"
+                    value={Organization}
+                    onChange={(e) => setOrganization(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className={org.inputgroup}>
+                  <i className={`${org.bx} ${org.bxmailsend}`}></i>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setemail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className={org.inputgroup}>
+                  <i className={`${org.bx} ${org.bxslockalt}`}></i>
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setpassword(e.target.value)}
+                    required
+                  />
+                </div>
+                {/* <div className={org.inputgroup}>
                 <i className={`${org.bx} ${org.bxslockalt}`}></i>
                 <input type="password" placeholder="Confirm password" />
               </div> */}
-              <button>Sign up</button>
-              <p>
-                <span>Already have an account?</span>
-                <b onClick={toggleForm} className={org.pointer}>
-                  Sign in here
-                </b>
-              </p>
-            </form>  
+                <button>Sign up</button>
+                <p>
+                  <span>Already have an account?</span>
+                  <b onClick={toggleForm} className={org.pointer}>
+                    Sign in here
+                  </b>
+                </p>
+              </form>
             </div>
           </div>
         </div>
@@ -202,28 +210,39 @@ const Organize = () => {
         >
           <div className={`${org.formwrapper} ${org.alignitemscenter}`}>
             <div className={`${org.form} ${org.signin}`}>
-            <form action="" onSubmit={handleLogin}> 
-              <div className={org.inputgroup}>
-                <p id="logmess"></p>
-                <i className={`${org.bx} ${org.bxsuser}`}></i>
-                <input type="text" placeholder="email" required value={Email} onChange={(e)=> setEmail(e.target.value)}/>
-              </div>
-              <div className={org.inputgroup}>
-                <i className={`${org.bx} ${org.bxslockalt}`}></i>
-                <input type="password" placeholder="Password" required value={Password} onChange={(e)=> setPassword(e.target.value)} />
-              </div>
-              <button type="submit">Sign in
-              </button>
-              <p>
-                <Link onClick={(e)=> setvisible(true)}>Forget password</Link>
-              </p>
-              <p>
-                <span>Don't have an account?</span>
-                <b onClick={toggleForm} className={org.pointer}>
-                  Sign up here
-                </b>
-              </p>
-            </form> 
+              <form action="" onSubmit={handleLogin}>
+                <div className={org.inputgroup}>
+                  <p id="logmess"></p>
+                  <i className={`${org.bx} ${org.bxsuser}`}></i>
+                  <input
+                    type="text"
+                    placeholder="email"
+                    required
+                    value={Email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className={org.inputgroup}>
+                  <i className={`${org.bx} ${org.bxslockalt}`}></i>
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={Password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <button type="submit">Sign in</button>
+                <p>
+                  <Link onClick={(e) => setvisible(true)}>Forget password</Link>
+                </p>
+                <p>
+                  <span>Don't have an account?</span>
+                  <b onClick={toggleForm} className={org.pointer}>
+                    Sign up here
+                  </b>
+                </p>
+              </form>
             </div>
           </div>
         </div>
@@ -263,32 +282,77 @@ const Organize = () => {
           style={{
             overlay: {
               zIndex: 2,
-              background: 'rgba(0, 0, 0, 0.75)',
-            }
-          }}  
+              background: "rgba(0, 0, 0, 0.75)",
+            },
+          }}
         >
-            <h1 style={{color:"black"}} className={org.retext}>RESET PASSWORD</h1>
-            <p id="emailcheck" style={{color:"black"}}></p>
-            <form action="" onClick={handleOtp}>
-              <div className={org.firotp}>
-                <input type="text" placeholder="Email"  className={org.inp} value={Emails} onChange={(e)=> setEmails(e.target.value)}/>
-                <button  className={org.otp} type="submit">Send OTP</button>
-              </div>
-            </form>
-            <p id="otp" style={{color:"green"}}></p>
-            <form action="" onSubmit={handleResetPassword}>
-              <div className={org.secotp}>
-              <input type="text" placeholder="OTP"  className={org.inp} value={OTP} onChange={(e)=> setOTP(e.target.value)}/>
-              <input type="hidden" placeholder="OTP"  className={org.inp} value={Emails}/>
-              <input type="text" placeholder="New Password"  className={org.inp} value={newPassword} onChange={(e)=> setnewPassword(e.target.value)}/>
-              </div>
-              <div style={{ display: 'flex', justifyContent: 'space-around', width: '100%' }}>
-                <button onClick={() => setvisible(false)} style={{ padding: '10px 20px', margin: '10px', }} className={org.close}>Close</button>
-                <button style={{ padding: '10px 20px', margin: '10px' }} className={org.sub} type="submit">Submit</button>
-              </div>
-            </form>
+          <h1 style={{ color: "black" }} className={org.retext}>
+            RESET PASSWORD
+          </h1>
+          <p id="emailcheck" style={{ color: "black" }}></p>
+          <form action="" onClick={handleOtp}>
+            <div className={org.firotp}>
+              <input
+                type="text"
+                placeholder="Email"
+                className={org.inp}
+                value={Emails}
+                onChange={(e) => setEmails(e.target.value)}
+              />
+              <button className={org.otp} type="submit">
+                Send OTP
+              </button>
+            </div>
+          </form>
+          <p id="otp" style={{ color: "green" }}></p>
+          <form action="" onSubmit={handleResetPassword}>
+            <div className={org.secotp}>
+              <input
+                type="text"
+                placeholder="OTP"
+                className={org.inp}
+                value={OTP}
+                onChange={(e) => setOTP(e.target.value)}
+              />
+              <input
+                type="hidden"
+                placeholder="OTP"
+                className={org.inp}
+                value={Emails}
+              />
+              <input
+                type="text"
+                placeholder="New Password"
+                className={org.inp}
+                value={newPassword}
+                onChange={(e) => setnewPassword(e.target.value)}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                width: "100%",
+              }}
+            >
+              <button
+                onClick={() => setvisible(false)}
+                style={{ padding: "10px 20px", margin: "10px" }}
+                className={org.close}
+              >
+                Close
+              </button>
+              <button
+                style={{ padding: "10px 20px", margin: "10px" }}
+                className={org.sub}
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
         </Model>
-      </div>  
+      </div>
     </div>
   );
 };
--- client/src/pages/Orghome.jsx
+++ client/src/pages/Orghome.jsx
@@ -2,38 +2,50 @@ import React from "react";
 import mod from "./Orghome.module.css";
 import { MdExitToApp } from "react-icons/md";
 import { FaBell } from "react-icons/fa";
-import { IoPersonCircle } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { MdEdit } from "react-icons/md";
+import { MdDeleteForever } from "react-icons/md";
+import Addpost from "./Addpost";
 
 const Orghome = () => {
   return (
     <div className={mod.org}>
-      <div className={mod.slidebar}>
-        <div className={mod.logo}>
-          <h2>Welcome Organization name</h2>
-        </div>
-        <div className={mod.header}>
-          <div className={mod.userprofile}>
-            <span className={mod.userprofilenotification}>
-              <FaBell />
-            </span>
-            <span className={mod.line}></span>
-            <button className={mod.logout}>
-              <Link to="/">
-                <span>Logout</span>
-                <span>
-                  <MdExitToApp />
-                </span>
-              </Link>
-            </button>
+      <div className={mod.nav}>
+        <div className={mod.slidebar}>
+          <div className={mod.logo}>
+            <h3>Welcome Organization name</h3>
+            <h2></h2>
+          </div>
+          <div className={mod.header}>
+            <div className={mod.userprofile}>
+              <span className={mod.userprofilenotification}>
+                <FaBell />
+              </span>
+              <span className={mod.line}></span>
+              <button className={mod.logout}>
+                <Link to="/">
+                  <span>Logout</span>
+                  <span className={mod.logouticon}>
+                    <MdExitToApp />
+                  </span>
+                </Link>
+              </button>
+            </div>
           </div>
         </div>
       </div>
       <hr />
       <div className={mod.maincontent}>
+        {/* <Banner /> */}
+        <br />
         <div className={mod.content}>
           <h1>My Posts</h1>
-          <button className={mod.addasset}>Add New Asset</button>
+          {/* <button className={mod.addasset}></button> */}
+          <Link to="/addpost" className={mod.addasset}>
+            Add New Post
+          </Link>
+          <br />
+          <br />
           <table>
             <thead>
               <tr>
@@ -52,8 +64,13 @@ const Orghome = () => {
                 <td>2 years</td>
                 <td>UG</td>
                 <td>10</td>
-                <td>
-                  <button>Edit</button> <button>Remove</button>
+                <td className={mod.options}>
+                  <button className={mod.optionedit}>
+                    <MdEdit className={mod.edit} />
+                  </button>
+                  <button className={mod.optiondelete}>
+                    <MdDeleteForever className={mod.delete} />
+                  </button>
                 </td>
               </tr>
             </tbody>
